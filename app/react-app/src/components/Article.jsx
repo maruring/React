3c8ae2354e82fa12188223f27e987454ea61a123
maruring <maruring3745@gmail.com>
2024-02-25 19:58:00 +0900

    propsにuseStateを渡す方法

--- app/react-app/src/components/Article.jsx
+++ app/react-app/src/components/Article.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useState } from "react";
-import {Title, Content} from './index';
+import {Title, Content, PublishButton} from './index';
 
 
 const Article = (props) => {
-    const [isPublish, setIsPublished] = useState(false);
-    console.log(isPublish);
+    const [isPublished, setIsPublished] = useState(false);
+    const publishArticle = () => {
+        setIsPublished(true);
+    };
     return (
         <div>
             <Title title={props.title} />
             <Content content={props.content} />
-            <button onClick={() => setIsPublished(true)}>公開</button>
+            <PublishButton isPublished={isPublished} onClick={publishArticle} />
         </div>
     );
 };
