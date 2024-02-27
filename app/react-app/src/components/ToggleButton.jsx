d06745fa02ac581d81e00f8786dcd6e9459153ee
maruring <maruring3745@gmail.com>
2024-02-27 19:23:41 +0900

    クリーンアップ関数を使ってみる

--- app/react-app/src/components/ToggleButton.jsx
+++ app/react-app/src/components/ToggleButton.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const ToggleButton = () =>{
     const [open, setOpen] = useState(true);
     const toggle = () => {
         setOpen(prevState => !prevState)
     };
+
+    useEffect(() => {
+        console.log("current state is", open)
+        if(open){
+            console.log("yes");
+        }
+        // ↓はクリーンアップ関数 -> 再レンダリングが実行される前に必ず実行される
+        return () => {
+            console.log("No");
+        }
+    })
 
     return(
         <button onClick={toggle}>
