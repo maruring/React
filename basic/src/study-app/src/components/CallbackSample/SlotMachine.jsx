fe245eba1224ade27ddf773acf6bae71749b84ad
maruring <maruring3745@gmail.com>
2024-03-10 10:12:39 +0900

    useCallbackを使用したslotmachine

--- basic/src/study-app/src/components/CallbackSample/SlotMachine.jsx
+++ basic/src/study-app/src/components/CallbackSample/SlotMachine.jsx
@@ -2,6 +2,7 @@ import React, {useState, useCallback, memo} from "react";
 
 // Button子コンポーネント
 const Button = memo((props) => {
+    console.log(props.position);
     return(
         <button type="button" onClick={props.handleClick}>
             {props.value}
@@ -11,13 +12,11 @@ const Button = memo((props) => {
 
 // 数が一致しているかを返却するコンポーネント
 const HitResult = memo((props) => {
-    console.log(props.leftNumber, props.centerNumber, props.rightNumber);
-    // const result = props.func(props.leftNumber, props.centerNumber, props.rightNumber);
-    const result = props.func;
-    console.log(result);
+    const isHit = props.leftNumber === props.centerNumber && props.centerNumber === props.rightNumber;
+    console.log('判定');
     return (
         <div>
-            {result ? <div>当たり</div> : <div>はずれ</div>}
+            {isHit ? <div>当たり</div> : <div>はずれ</div>}
         </div>
     );
 });
@@ -27,27 +26,21 @@ const getNumber = () => {
     return num;
 };
 
-const getHitJudgement = (leftNumber, centerNumber, rightNumber) => {
-    return leftNumber === centerNumber && centerNumber === rightNumber;
-};
-
 const SlotMachine = () => {
     const [leftNumber, setLeftNumber] = useState(getNumber);
     const [centerNumber, setCenterNumber] = useState(getNumber);
     const [rightNumber, setRightNumber] = useState(getNumber);
-    const [hit, setHit] = useState(false);
 
     const stopLeftNumber = useCallback(() => setLeftNumber(getNumber), [leftNumber]);
     const stopCenterNumber = useCallback(() => setCenterNumber(getNumber), [centerNumber]);
     const stopRightNumber = useCallback(() => setRightNumber(getNumber), [rightNumber]);
-    const judgementHit = useCallback(() => setHit(getHitJudgement(leftNumber, centerNumber, rightNumber)), [leftNumber, centerNumber, rightNumber]);
 
     return(
         <div>
             <Button position='left' handleClick={stopLeftNumber} value={leftNumber} />
             <Button position='center' handleClick={stopCenterNumber} value={centerNumber} />
             <Button position='right' handleClick={stopRightNumber} value={rightNumber} />
-            <HitResult func={judgementHit} judgement={hit} leftNumber={leftNumber} centerNumber={centerNumber} rightNumber={rightNumber} />
+            <HitResult leftNumber={leftNumber} centerNumber={centerNumber} rightNumber={rightNumber} />
         </div>
     );
 };
